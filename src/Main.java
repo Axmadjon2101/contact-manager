16255a83da394f00c229ac6376df8752be14344b
Axmadjon2101 <Ahmadjon.210@icloud.com>
2026-07-14 19:26:43 +0500

    Add version method to Main class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-
 public class Main {
 
     public static void main(String[] args) {
         ContactManager contactManager = new ContactManager();
         contactManager.start();
+    }
+
+    public static void version() {
+        System.out.println("Version 1.0");
     }
 }
