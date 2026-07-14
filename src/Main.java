765415ff6bdf5709a3b8e8497ea90a5d68c5ae6a
Ahmadjon <axmadjonyusupov305@gmail.com>
2026-07-14 12:37:17 +0500

    Remove unused static variables from Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 
 public class Main {
-    static Contact[] contactArray = new Contact[2];
-    static int index = 0;
 
     public static void main(String[] args) {
         ContactManager contactManager = new ContactManager();
